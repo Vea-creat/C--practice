994c3bb8f007722ae3d9332b7fe51347a3dbf952
Vea-creat <2297983058@qq.com>
2026-09-07 22:31:22 +0800

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+using namespace std;
+void test01() {
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	cout << "a的体重是" << endl;
+	cin >> a;
+	cout << "b的体重是" << endl;
+	cin >> b;
+	cout << "c的体重是" << endl;
+	cin >> c;
+	if (a > b) {
+		if (a > c) {
+			cout << "a最重" << endl;
+		}
+		else if (a == c) {
+			cout << "a和b最重" << endl;
+		}
+		else {
+			cout << "c最重" << endl;
+		}
+	}
+	else if(a<b){
+		if (b > c) {
+			cout << "b最重" << endl;
+		}
+		else if (b == c) {
+			cout << "b和c最重" << endl;
+		}
+		else {
+			cout << "c最重" << endl;
+		}
+	}
+	else {
+		if (a > c) {
+			cout << "a和b最重" << endl;
+		}
+		else if(a == c) {
+			cout << "三只一样重" << endl;
+		}
+		else {
+			cout << "c最重" << endl;
+		}
+	}
+}
+int main() {
+	test01();
+}
